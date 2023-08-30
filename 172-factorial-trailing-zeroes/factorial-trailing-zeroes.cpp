d44932fa56d55ce95b58cb12d2acad2bc78d2bf3
Chaman Pandey <97782172+dbook01@users.noreply.github.com>
2023-08-30 23:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (90.84%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int sum = 0 ;
-        while(n>=5){
-            sum+=n/5;
-            n/=5;
-        }
-
-        return sum;
+        return n/5 + n/25 + n/125 + n/625 + n/3125;
     }
 };
